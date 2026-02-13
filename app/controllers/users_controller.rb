2aab8ae32044c997ff1f71b94dd9b066f34757fd
tenny <tzvaita@gmail.com>
2026-02-13 12:39:55 +0200

    Add user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+end
